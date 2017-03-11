7d5c6e5c9a18157f749dc1ae4f97f2172cdc40d7
Leo Feradero Nugraha <leoferaderonugraha@yahoo.com>
2017-03-12 04:10:36 +0700

    Update typewriter.py

--- some_programs/typewriter.py
+++ some_programs/typewriter.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
+'''TypeWriter is an cool effect, Created by rion'''
 
 from __future__ import print_function
 from time import sleep
 from random import randint as rand
 import sys
 
-txt = "TypeWriter is an amazing effect,\n Created by rion :)\n"
-for i in txt:
+if(len(sys.argv) != 2):
+	print("Usage <%s> <filename>" % sys.argv[0], end='\n')
+	sys.exit()
+fp = open(sys.argv[1], 'r').read()
+for i in fp:
 	print(i, end='')
 	sleep(rand(0,1)/10.)
 	sys.stdout.flush()
+sleep(0.3)
