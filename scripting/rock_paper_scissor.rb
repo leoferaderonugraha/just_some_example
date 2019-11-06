c0a0946dcf575e0c6aaee7736bf02fc4bac2c6bb
Leo Feradero Nugraha <leoferaderonugraha@gmail.com>
2019-11-07 06:53:19 +0700

    Update and rename rock_paper_scissor.rb to scripting/rock_paper_scissor.rb

--- scripting/rock_paper_scissor.rb
+++ scripting/rock_paper_scissor.rb
@@ -3,17 +3,20 @@ print 'What\'s your choice: '
 
 u_choice = gets.chomp.to_i
 bot_choice = rand(0...3) #gets.chomp.to_i 
+# This is simple rule that i found
+# while writing the first output of this program.
+# Basically {'user' => [1,-2], 'bot' => [-1, 2]}
 result = u_choice - bot_choice
 
 choices = ['Rock','Paper','Scissor']
 
-puts "Your: #{choices[u_choice-1]}\nBot: #{choices[bot_choice-1]}"
+puts "You: #{choices[u_choice-1]}\nBot: #{choices[bot_choice-1]}"
 
 case result
 when 0
   puts 'Draw'
 when 1, -2
-  puts 'User Win'
+  puts 'You Win'
 when 2,-1
   puts 'Bot Win'
 else
